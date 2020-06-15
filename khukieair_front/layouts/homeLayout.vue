463e1309f6fc5f6e01a89221cd7ffb3348422cb8
최원영 <wonting43@khu.ac.kr>
2020-06-15 19:35:20 +0900

    front

---
+++ khukieair_front/layouts/homeLayout.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-app light>
+    <v-navigation-drawer
+      app
+      absolute
+      permanent
+      right
+      color="brown darken-2"
+      width="180px"
+    >
+      <template v-slot:prepend>
+        <v-list-item three-line />
+        <v-list-item three-line>
+          <nuxt-link to="/edit/">
+            <v-list-item-avatar>
+              <img src="../assets/monster.png">
+            </v-list-item-avatar>
+          </nuxt-link>
+          <v-list-item-content>
+            <v-list-item-title> User  </v-list-item-title>
+            <v-list-item-subtitle>Logged In!</v-list-item-subtitle>
+            <v-list-item-subtitle>
+              <nuxtLink to="/">
+                Sign out
+              </nuxtLink>
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+      <v-divider class="mx-4" />
+      <v-card-actions>
+        <v-btn small color="brown" @click.stop="dialog = true">
+          <v-icon left small>
+            mdi-file-upload
+          </v-icon> File Upload <v-icon color="brown" small>
+            file
+          </v-icon>
+        </v-btn>
+      </v-card-actions>
+      <v-dialog v-model="dialog" max-width="700">
+        <v-card color="brown">
+          <v-card-actions>
+            <v-file-input small-chips multiple label="File Upload" /> <!-- 파일 인풋-->
+            <v-spacer />
+            <v-btn text @click="dialog = false">
+              Upload
+            </v-btn>
+            <v-btn text @click="dialog = false">
+              cancle
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-divider class="mx-4" />
+      <v-card-actions>
+        <v-btn color="brown" small>
+          <v-icon left medium>
+            mdi-account-arrow-right
+          </v-icon>
+          Delete  Account
+        </v-btn>
+      </v-card-actions>
+    </v-navigation-drawer>
+    <v-app-bar
+      app
+      clipped-right
+      color="brown darken-4"
+      height="90"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        <nuxt-link to="/home/">
+          <img src="../assets/logo.png" height="85px">
+        </nuxt-link>
+      </v-col>
+    </v-app-bar>
+
+    <v-content>
+      <v-container>
+        <nuxt /> <!-- 이부분이 핵심 index.vue가 보여지는 부분이다. -->
+      </v-container>
+    </v-content>
+
+    <v-footer
+      height="55"
+      absolute
+    >
+      <v-col>
+        {{ new Date().getFullYear() }}
+        —
+        <v-icon small color="brown lighten-2">
+          mdi-cookie
+        </v-icon>
+        Khukie_air
+        <v-icon small>
+          mdi-feather
+        </v-icon>
+      </v-col>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      dialog: false,
+      items: [
+      ]
+    }
+  }
+}
+</script>
---
+++ khukieair_front/pages/signup.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="signform">
+      <h2>Sign up to KHUKIE</h2>
+      <form action="" method="POST">
+        <input type="text" name="firstname" placeholder="First name">
+        <br>
+        <input type="text" name="lastname" placeholder="Last name">
+        <br>
+        <input type="text" name="email" placeholder="E-mail">
+        <br>
+        <input type="password" name="password" placeholder="Password">
+        <br>
+        <btn type="submit">
+          가입
+        </btn>
+      </form>
+    </div>
+    <div class="signform-help">
+      <a href="/signin">기존 계정으로 로그인</a>
+    </div>
+    <div class="warning">
+      test msg!!
+    </div>
+    <div class="line" />
+    <div class="OAuth">
+      <btn type="button" onclick="">
+        <img src="~/assets/google.png">
+        구글 계정으로 가입
+      </btn>
+      <btn type="button" onclick="">
+        <img src="~/assets/kakao.png">
+        카카오 계정으로 가입
+      </btn>
+    </div>
+  </div>
+</template>
