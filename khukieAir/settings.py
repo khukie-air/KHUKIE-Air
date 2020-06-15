88569002d212ee03a0ee2b300ceffd86978fcaf0
Freckie <freckie@frec.kr>
2020-06-16 00:46:18 +0900

    Update: now use mysql on rds

--- khukieAir/settings.py
+++ khukieAir/settings.py
@@ -99,14 +99,22 @@ REST_FRAMEWORK = {
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': KHUKIEAIR_CONFIG['aws']['rds']['endpoint'],
+        'PORT': KHUKIEAIR_CONFIG['aws']['rds']['port'],
+        'NAME': KHUKIEAIR_CONFIG['aws']['rds']['schema'],
+        'USER': KHUKIEAIR_CONFIG['aws']['rds']['user'],
+        'PASSWORD': KHUKIEAIR_CONFIG['aws']['rds']['password'],
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
